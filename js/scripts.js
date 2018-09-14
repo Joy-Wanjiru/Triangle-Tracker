59ecc911912e2bd0087512c8ea6af1797a5a7574
Joy-Wanjiru <wanjirujoymuhia@gmail.com>
2018-09-14 13:05:20 +0300

    Add styles

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+alert
+var triangleperimeter = [sideAB + sideAC + sideBC];
+  if sideAB==sideAC && sideAB==sideBC && sideAC==sideBC;  {
+    alert ("Awesome! You just created an Equilateral triangle. All the sides of the triangle are equal.")
+}
+  else if sideAB==sideAC && sideAB==sideBC && sideBC!==sideAB; {
+    alert ("Great work! That is an isosceles triangle. Two sides are equal.")
+  }
+  else if sideBC==sideAB && sideBC=sideAC && sideAB!==sideAC; {
+    alert ("Great work! That is an isosceles triangle. Two sides are equal.")
+  }
+  else if sideAB!==sideAC!==sideBC; {
+    alert ("Kudos! You have made a scalene triangle. No side is equal to the other.")
+  }
+  else {
+
+  }
